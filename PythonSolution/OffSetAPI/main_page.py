f2551a3a5ed498ae0e5bdf7f46a00b6ac5151dd6
Rafael Lúcio <rafaellucio@pm.me>
2021-05-18 16:47:12 -0300

    Root page html first code

---
+++ PythonSolution/OffSetAPI/main_page.py
@@ -0,0 +1,8 @@
+root_html_body = """
+<body>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+    <input name="files" type="file" multiple>
+    <input type="submit">
+</form>
+</body>
+"""

66834df5e6e16ed99c3faa8fff526e90f0e67512
Rafael Lúcio <rafaellucio@pm.me>
2021-05-18 20:19:09 -0300

    Receive DB entry and save it in a file

--- PythonSolution/OffSetAPI/file_processing/uploaded_files.py
+++ PythonSolution/OffSetAPI/file_processing/uploaded_files.py
@@ -1,7 +1,10 @@
+import os
 import shutil
 from fastapi import UploadFile
 from pathlib import Path
-from tempfile import NamedTemporaryFile
+from tempfile import NamedTemporaryFile, SpooledTemporaryFile
+
+from OffSetAPI.db.model import Subtitles
 
 
 def save_upload_file_tmp(upload_file: UploadFile) -> Path:
@@ -13,3 +16,15 @@ def save_upload_file_tmp(upload_file: UploadFile) -> Path:
     finally:
         upload_file.file.close()
     return tmp_path
+
+def save_db_file_tmp(db_entry: Subtitles) -> str:
+    if os.path.exists('cache/'):
+        pass
+    else:
+        os.mkdir('cache/')
+
+    file_path = f'cache/{db_entry.file_name}'
+    with open(file_path, mode = 'w') as f:
+        f.write(db_entry.file_content)
+
+    return file_path

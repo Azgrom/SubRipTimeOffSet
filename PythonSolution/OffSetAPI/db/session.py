71cd81e3219f9dc2cc07c93a14662b43d08946b3
Rafael Lúcio <rafaellucio@pm.me>
2021-05-18 16:48:13 -0300

    DB CRUD methods

---
+++ PythonSolution/OffSetAPI/db/session.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import Session
+from typing import Optional
+
+from . import model, schema
+
+
+class InitializeSession:
+    def __init__(self, db: Session, skip: Optional[int] = 0,
+                 limit: Optional[int] = 100):
+        self.db = db
+        self.skip = skip
+        self.limit = limit
+
+
+class GetMethods(InitializeSession):
+    def get_title_occurrence(self, title: str, db_field: str):
+        title_filter = db_field == title
+        title_occurence = self.db.query(model.Subtitles).filter(title_filter)
+        return title_occurence.offset(self.skip).limit(self.limit).all()
+
+    def list_db_by_title(self, title: str):
+        field = model.Subtitles.file_name
+        return self.get_title_occurrence(title, field)
+
+
+class PostMethods(InitializeSession):
+    def register_subtitle(self, sub_schema: schema.SubTitleSchema):
+        db_subt = model.Subtitles(**sub_schema.dict())
+        self.db.add(db_subt)
+        self.db.commit()
+        self.db.refresh(db_subt)
+        return db_subt

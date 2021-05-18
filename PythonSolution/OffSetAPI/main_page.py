4ed9be24730c3a6279b718129da0dd20a4444c2f
Rafael Lúcio <rafaellucio@pm.me>
2021-05-18 20:15:59 -0300

    download_subtitle_by_id route button

--- PythonSolution/OffSetAPI/main_page.py
+++ PythonSolution/OffSetAPI/main_page.py
@@ -1,7 +1,11 @@
 root_html_body = """
 <body>
-<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-    <input name="files" type="file" multiple>
+<form action="/upload_to_db/" enctype="multipart/form-data" method="post">
+    <input name="file" type="file" multiple>
+    <input type="submit">
+</form>
+<form action="/download_subtitle_by_id/" enctype="multipart/form-data" method="get">
+    <input type="number" name="id">
     <input type="submit">
 </form>
 </body>

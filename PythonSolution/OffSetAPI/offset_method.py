9a1d8e718b076e14a5f103bf6cdafa025e7dab0f
Rafael Lúcio <rafaellucio@pm.me>
2021-05-18 12:07:49 -0300

    Subtitle time offset method

---
+++ PythonSolution/OffSetAPI/offset_method.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 09:57:31 2021
+
+@author: Rafael Lúcio
+"""
+
+import srt
+import datetime as dt
+
+def offset_sub(sub_file_path: str, offset_in_secs: float):
+    with open(sub_file_path, 'r') as f:
+        data = f.read()
+
+    for sub in list(srt.parse(data)):
+        sub.start += dt.timedelta(seconds = offset_in_secs)
+        sub.end += dt.timedelta(seconds = offset_in_secs)
+
+    with open(sub_file_path, 'w') as f:
+        f.write(srt.compose(sub))

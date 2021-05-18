bc008141729481a94ba1c83f48e675764c909aaf
Rafael Lúcio <rafaellucio@pm.me>
2021-05-18 16:48:26 -0300

    Rename file

---
+++ PythonSolution/OffSetAPI/db/model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 11:03:23 2021
+
+@author: Rafael Lúcio
+"""
+
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, Time
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class Subtitles(Base):
+    __tablename__ = "subtitle_submissions"
+
+    id = Column(Integer, primary_key = True, index = True)
+    file_name = Column(String, unique = True)
+    file_content = Column(Text)

3c21d4f09819978f91f9cd0fe003e023b261d33d
Rafael Lúcio <rafaellucio@pm.me>
2021-05-18 21:05:26 -0300

    Fix bad implementation

--- PythonSolution/OffSetAPI/file_processing/offset_method.py
+++ PythonSolution/OffSetAPI/file_processing/offset_method.py
@@ -9,13 +9,14 @@ Created on Tue May 18 09:57:31 2021
 import srt
 import datetime as dt
 
-def offset_sub(subtitle_file_path: str, offset_in_secs: float):
+def offset_sub(subtitle_file_path: str, offset_in_secs: float = 0):
     with open(subtitle_file_path, 'r') as f:
         data = f.read()
 
-    for sub in list(srt.parse(data)):
+    subs = list(srt.parse(data))
+    for sub in subs:
         sub.start += dt.timedelta(seconds = offset_in_secs)
         sub.end += dt.timedelta(seconds = offset_in_secs)
 
     with open(subtitle_file_path, 'w') as f:
-        f.write(srt.compose(sub))
+        f.write(format(srt.compose(subs)))

1b4bec67a2a1c99fc2778ecf756cf8b3dee91958
Rafael Lúcio <rafaellucio@pm.me>
2021-05-20 13:53:42 -0300

    Timestamp identifier tests

--- RustSolution/src/main.rs
+++ RustSolution/src/main.rs
@@ -18,26 +18,45 @@ fn main() {
         },
     };
 
-    println!("{:?}", file.filename);
-    println!("{:?}", file.contents);
+    let example = file.contents.lines();
 }
 
-#[cfg(tests)]
+#[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn count_indexes() {
-        let amount: u8 = 2;
-        let example = "\
-1
-00:02:17,440 --> 00:02:20,375
-Senator, we're making
-our final approach into Coruscant.
+    fn timestamp_splitter_asserter(timestamp_string: String, expected_result: Vec<&str>) {
+        let obtained_result: Vec<&str> = timestamp_splitter(&timestamp_string);
 
-2
-00:02:20,476 --> 00:02:22,501
-Very good, Lieutenant.";
-        assert_eq!(amount, index_counter(example));
+        assert_eq!(expected_result, obtained_result);
     }
+
+    #[test]
+    fn timestamp_splitter_tester() {
+        let timestamp_string: String = "00:02:17,440 --> 00:02:20,375".to_string();
+        let expected_result: Vec<&str> = ["00:02:17,440", "00:02:20,375"].to_vec();
+        timestamp_splitter_asserter(timestamp_string, expected_result);
+
+        let timestamp_string: String = "00:02:20,476 --> 00:02:22,501".to_string();
+        let expected_result: Vec<&str> = ["00:02:20,476", "00:02:22,501"].to_vec();
+        timestamp_splitter_asserter(timestamp_string, expected_result);
+
+        let timestamp_string: String = "00:01:20,439 --> 00:01:22,479".to_string();
+        let expected_result: Vec<&str> = ["00:01:20,439", "00:01:22,479"].to_vec();
+        timestamp_splitter_asserter(timestamp_string, expected_result);
+    }
+    //     #[test]
+    //     fn count_indexes() {
+    //         let amount: u8 = 2;
+    //         let example = "\
+    // 1
+    // 00:02:17,440 --> 00:02:20,375
+    // Senator, we're making
+    // our final approach into Coruscant.
+    //
+    // 2
+    // 00:02:20,476 --> 00:02:22,501
+    // Very good, Lieutenant.";
+    //         assert_eq!(amount, index_counter(example));
+    //     }
 }

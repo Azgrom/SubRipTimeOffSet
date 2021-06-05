e2dbc62a31237042ba108ab1f3601b705c9411dc
Rafael Lúcio <rafaellucio@pm.me>
2021-06-05 17:40:15 -0300

    `cargo fmt` thing

--- RustSolution/src/bin/write_post.rs
+++ RustSolution/src/bin/write_post.rs
@@ -1,5 +1,5 @@
-extern crate diesel_demo;
 extern crate diesel;
+extern crate diesel_demo;
 
 use self::diesel_demo::*;
 use std::io::{stdin, Read};
@@ -12,7 +12,10 @@ fn main() {
     stdin().read_line(&mut title).unwrap();
 
     let title = &title[..(title.len() - 1)]; // Drop the newline character
-    println!("\nOk! Let's write '{}' (Press {} when finished)\n", title, EOF);
+    println!(
+        "\nOk! Let's write '{}' (Press {} when finished)\n",
+        title, EOF
+    );
 
     let mut body = String::new();
     stdin().read_to_string(&mut body).unwrap();

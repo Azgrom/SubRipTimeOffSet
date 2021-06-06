a621a6f3224e431bbe6c83f6c6deff575c514294
Rafael Lúcio <rafaellucio@pm.me>
2021-06-05 23:27:34 -0300

    `show_posts` lib function definition

--- RustSolution/src/lib.rs
+++ RustSolution/src/lib.rs
@@ -34,6 +34,25 @@ pub fn create_post<'a>(conn: &SqliteConnection, title: &'a str, body: &'a str) -
         .expect("Error saving new post")
 }
 
+pub fn show_posts() {
+    use diesel_demo::schema::posts::dsl::*;
+
+    let connection = establish_connection();
+    let results = posts
+        .filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(&connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+
+    for post in results {
+        println!("{}", post.title);
+        println!("----------\n");
+        println!("{}", post.body);
+    }
+}
+
 pub fn delete_post() {
     use diesel_demo::schema::posts::dsl::*;
 

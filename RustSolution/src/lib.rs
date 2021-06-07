f5533684010f91fea74d37176390cb90e38a06d5
Rafael Lúcio <rafaellucio@pm.me>
2021-06-07 19:40:57 -0300

    Create function for the SubRipFile content stringfier routine

--- RustSolution/src/lib.rs
+++ RustSolution/src/lib.rs
@@ -2,3 +2,16 @@ mod subrip;
 
 pub use subrip::SubRipFile;
 pub use sqlite_diesel::crud;
+impl SubRipFile {
+    fn content_string_parser(&self) -> String {
+        let mut content_string = String::new();
+
+        for content in self.contents.iter().enumerate() {
+            content_string.push_str(&(content.0 + 1).to_string());
+            content_string.push('\n');
+            content_string.push_str(&content.1.to_string());
+        }
+
+        content_string
+    }
+}

7d655a68a50788df53b1aa1dfad86432588896de
Rafael Lúcio <rafaellucio@pm.me>
2021-06-07 15:59:35 -0300

    Deal with different results for `SubRipFile::new()`

--- RustSolution/src/bin/main.rs
+++ RustSolution/src/bin/main.rs
@@ -1,9 +1,15 @@
 use std::{env, process};
-use subrip_offset_api::{SubRipFile, TimeStamp};
+use subrip_offset_api::SubRipFile;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut parsed_file_content = SubRipFile::new(args);
+    let mut parsed_file_content = match SubRipFile::new(args) {
+        Ok(parsed_file_content) => parsed_file_content,
+        Err(err) => {
+            println!("Problem parsing arguments: {}", err);
+            process::exit(1);
+        }
+    };
 
     println!("{}", parsed_file_content);
 

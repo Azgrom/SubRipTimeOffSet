fe2b741dfa36d0cc1b712aa2293339d058d5f3ee
Rafael Lúcio <rafaellucio@pm.me>
2021-06-13 14:51:24 -0300

    Receive and print file

--- RustSolution/src/main.rs
+++ RustSolution/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use std::io;
+use rocket::data::{Data, ToByteUnit};
+
+#[post("/", data = "<data>")]
+async fn data_guard(mut data: Data<'_>) -> io::Result<Vec<u8>> {
+    println!("All of the data: {:?}", data.peek(512).await);
+
+    let bytes = data.open(200.kibibytes()).into_bytes().await?;
+    if !bytes.is_complete() {
+        println!("there are bytes remaining in the stream");
+    }
+
+    // println!("{:?}", std::str::from_utf8(&bytes.value));
+
+    Ok(bytes.into_inner())
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![data_guard])
 }

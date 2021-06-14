343b81fd1b417000b7d3fe0d291f8b2ecca73afe
Rafael Lúcio <rafaellucio@pm.me>
2021-06-13 22:21:55 -0300

    `cargo fmt` thing

--- RustSolution/src/main.rs
+++ RustSolution/src/main.rs
@@ -1,35 +1,24 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use std::{io, env, process};
-use subrip::SubRipFile;
 use rocket::{data::Capped, fs::TempFile};
+use std::{env, io, process};
+use subrip::SubRipFile;
 
 const FILE_NAME: &str = "rocket_tmp.srt";
 
-
 #[post("/file", data = "<file>")]
 async fn upload(mut file: Capped<TempFile<'_>>) -> io::Result<String> {
-
     if file.is_complete() {
-        let complete_path = format!("rocket-app/complete/{}", FILE_NAME);
+        let complete_path = format!("complete_{}", FILE_NAME);
         file.persist_to(env::temp_dir().join(complete_path)).await?;
     } else {
-        let incomplete_path = format!("rocket-app/incomplete/{}", FILE_NAME);
-        file.persist_to(env::temp_dir().join(incomplete_path)).await?;
+        let incomplete_path = format!("incomplete_{}", FILE_NAME);
+        file.persist_to(env::temp_dir().join(incomplete_path))
+            .await?;
     }
 
     let temp_file_path = file.path().unwrap().display();
-
-    // println!("{} bytes at {}", file.n.written, temp_file_path);
-
-    // let test = match SubRipFile::new(temp_file_path.to_string()) {
-    //     Ok(file_content) => file_content,
-    //     Err(err) => {
-    //         println!("Problem parsing arguments: {}", err);
-    //         process::exit(1);
-    //     }
-    // };
-    // println!("{}", test);
 
     Ok(format!("{} bytes at {}", file.n.written, temp_file_path))
 }

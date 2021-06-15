e899ab3722933f834c6637f64a117923ae048001
Rafael Lúcio <rafaellucio@pm.me>
2021-06-14 23:51:06 -0300

    Some custom config tweaks

--- RustSolution/src/main.rs
+++ RustSolution/src/main.rs
@@ -4,5 +4,9 @@ use subrip_web_api::{upload, file};
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![upload, file])
+    let figment = rocket::Config::figment()
+        .merge(("address", "0.0.0.0"))
+        .merge(("port", 1111));
+
+    rocket::custom(figment).mount("/", routes![upload, file])
 }

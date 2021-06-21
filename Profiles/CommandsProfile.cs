b40d4648d2e8bd5130e103fe6a422760fad27819
Rafael Lúcio <rafaellucio@pm.me>
2021-06-21 15:37:53 -0300

    Commander `Mapper` profile

---
+++ Profiles/CommandsProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Commander.Dtobjs;
+using Commander.Models;
+
+namespace Commander.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Command, CommandReadDtobj>();
+        }
+    }
+}

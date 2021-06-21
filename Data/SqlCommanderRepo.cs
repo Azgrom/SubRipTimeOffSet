94cf1cbac544fb193c598a09190231fed2b0b387
Rafael Lúcio <rafaellucio@pm.me>
2021-06-21 17:01:41 -0300

    Registry validation with `null` exception branch

--- Data/SqlCommanderRepo.cs
+++ Data/SqlCommanderRepo.cs
@@ -1,4 +1,5 @@
 using Commander.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,6 +12,16 @@ namespace Commander.Data
         public SqlCommanderRepo(CommanderContext context)
         {
             _context = context;
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            if (cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+
+            _context.Commands.Add(cmd);
         }
 
         public IEnumerable<Command> GetAllCommands()

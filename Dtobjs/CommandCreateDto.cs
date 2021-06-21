cae414559ce41f0449a1ff2a7aeed366f2c3b3a8
Rafael Lúcio <rafaellucio@pm.me>
2021-06-21 16:56:00 -0300

    Required fields for a new object

---
+++ Dtobjs/CommandCreateDto.cs
@@ -0,0 +1,11 @@
+namespace Commander.Dtobjs
+{
+    public class CommandCreateDtobj
+    {
+        public string HowTo { get; set; }
+
+        public string Line { get; set; }
+
+        public string Platform { get; set; }
+    }
+}

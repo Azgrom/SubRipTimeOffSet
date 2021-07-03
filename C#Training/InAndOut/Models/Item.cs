67c971e3f9c3e7f7badf5e2790fc99d926765041
Rafael Lúcio <rafaellucio@pm.me>
2021-07-03 15:44:04 -0300

    Created `id` column model method

--- C#Training/InAndOut/Models/Item.cs
+++ C#Training/InAndOut/Models/Item.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +7,6 @@ namespace InAndOut.Models
 {
     public class Item
     {
+        public int Id { get; set; }
     }
 }

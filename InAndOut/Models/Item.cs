ad98d776c97f15c69f77612637f86c91919caaab
Rafael Lúcio <rafaellucio@pm.me>
2021-07-04 12:50:18 -0300

    Code readability style

--- InAndOut/Models/Item.cs
+++ InAndOut/Models/Item.cs
@@ -7,8 +7,11 @@ namespace InAndOut.Models
     {
         [Key]
         public int Id { get; set; }
+
         public string Borrower { get; set; }
+
         public string Lender { get; set; }
+
         [DisplayName("Item name")]
         public string ItemName { get; set; }
     }

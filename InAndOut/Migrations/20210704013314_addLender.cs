e9f6ec9e5e384dff625e0747ac3acd1db43a87ed
Rafael Lúcio <rafaellucio@pm.me>
2021-07-03 22:36:19 -0300

    Lender field DB migration

---
+++ InAndOut/Migrations/20210704013314_addLender.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InAndOut.Migrations
+{
+    public partial class addLender : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Lender",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Lender",
+                table: "Items");
+        }
+    }
+}

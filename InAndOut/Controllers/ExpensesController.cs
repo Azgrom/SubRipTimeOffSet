93779d9a17cc3916e951e704a84878cbe5e1c761
Rafael Lúcio <rafaellucio@pm.me>
2021-07-04 12:51:51 -0300

    Expense controller with DB context wrapper

---
+++ InAndOut/Controllers/ExpensesController.cs
@@ -0,0 +1,25 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public ExpensesController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objList = _db.Expenses;
+            return View();
+        }
+    }
+}

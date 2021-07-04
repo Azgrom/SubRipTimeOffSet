429e95d40ab3615503af9241333225c45665d618
Rafael Lúcio <rafaellucio@pm.me>
2021-07-04 15:05:59 -0300

    Convert `Amount` field to `int`

---
+++ InAndOut/Migrations/20210704180119_ExpenseTable.int.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InAndOut.Migrations
+{
+    public partial class ExpenseTableint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpenseName",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Amount",
+                table: "Expenses",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpenseName",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Amount",
+                table: "Expenses",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}

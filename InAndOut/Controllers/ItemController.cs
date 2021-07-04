7f786ca555d3b1cd2eddd13c22d5f58626b94087
Rafael Lúcio <rafaellucio@pm.me>
2021-07-04 12:48:13 -0300

    `POST` Item controller method

--- InAndOut/Controllers/ItemController.cs
+++ InAndOut/Controllers/ItemController.cs
@@ -25,5 +25,15 @@ namespace InAndOut.Controllers
         {
             return View();
         }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+            _db.Items.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }

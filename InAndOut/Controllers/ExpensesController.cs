7235c691989a82f2aef6c35a3ad2ffac4736b12c
Rafael Lúcio <rafaellucio@pm.me>
2021-07-04 13:18:30 -0300

    Create Expense page `POST` request

--- InAndOut/Controllers/ExpensesController.cs
+++ InAndOut/Controllers/ExpensesController.cs
@@ -29,5 +29,15 @@ namespace InAndOut.Controllers
         {
             return View();
         }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense obj)
+        {
+            _db.Expenses.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }

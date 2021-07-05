087a558e3826d88ead87878e09eebaf102654e52
Rafael Lúcio <rafaellucio@pm.me>
2021-07-04 22:37:47 -0300

    `Expenses` FK of `ExpenseType` migrations

---
+++ InAndOut/Migrations/20210705004923_AddExpenseTypeRequiredForeignKey.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InAndOut.Migrations
+{
+    public partial class AddExpenseTypeRequiredForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExpenseIdType",
+                table: "Expenses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_ExpenseIdType",
+                table: "Expenses",
+                column: "ExpenseIdType");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_ExpenseTypes_ExpenseIdType",
+                table: "Expenses",
+                column: "ExpenseIdType",
+                principalTable: "ExpenseTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_ExpenseTypes_ExpenseIdType",
+                table: "Expenses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Expenses_ExpenseIdType",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "ExpenseIdType",
+                table: "Expenses");
+        }
+    }
+}

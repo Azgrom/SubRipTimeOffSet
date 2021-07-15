16f4d6c0944cd5348922b49237d6045471e8affd
Rafael Lúcio <rafaellucio@pm.me>
2021-07-14 22:22:22 -0300

    Create `Expense` ViewModel wrapper to deal along with `ExpenseType`

---
+++ InAndOut/Models/ViewModels/ExpenseVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace InAndOut.Models.ViewModels
+{
+    public class ExpenseVM
+    {
+        public Expense Expense { get; set; }
+
+        public IEnumerable<SelectListItem> TypeDropDown { get; set; }
+    }
+}

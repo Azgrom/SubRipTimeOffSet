e584081d89ec9ce4aaf568d268ce33249daeec57
Rafael Lúcio <rafaellucio@pm.me>
2021-07-14 22:27:31 -0300

    `ExpenseVM` import

--- InAndOut/Controllers/ExpensesController.cs
+++ InAndOut/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using InAndOut.Data;
 using InAndOut.Models;
+using InAndOut.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;

a3471bb3b053bf62a6a0b6d113a6ba5fcb483536
Rafael Lúcio <rafaellucio@pm.me>
2021-07-18 00:46:23 -0300

    Substitute .batch files do docker compose YAML

--- weatherapi/run-docker.bat
+++
@@ -1,1 +0,0 @@
-docker run -p 5000:80 -p 5001:443 -e ASPNETCORE_URLS="https://+;http://+" -e ASPNETCORE_HTTPS_PORT=5001 -e ASPNETCORE_ENVIRONMENT=Development -v %APPDATA%\Microsoft\UserSecrets\:/root/.microsoft/usersecrets -v %USERPROFILE%\.aspnet\https:/root/.aspnet/https/ weatherapi:1.0

038e66d77d50aef20e0c4ab9e4ea0e2cc339c645
Rafael Lúcio <rafaellucio@pm.me>
2021-07-17 23:51:27 -0300

    Running configuration

--- weatherapi/run-docker.bat
+++ weatherapi/run-docker.bat
@@ -1,1 +1,1 @@
-docker run -p 5000:80 weatherapi:1.0
+docker run --name weather -p 5000:80 -p 5001:443 -e ASPNETCORE_URLS="https://+;http://+" -e ASPNETCORE_HTTPS_PORT=5001 -e ASPNETCORE_ENVIRONMENT=Development -v $env:APPDATA\Microsoft\UserSecrets\:/root/.microsoft/usersecrets -v $env:USERPROFILE\.aspnet\https:/root/.aspnet/https weatherapi:1.0

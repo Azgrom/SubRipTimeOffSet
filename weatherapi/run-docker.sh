dcba4d6271c400bb2857a5a4db5b04ee7089a06a
Rafael Lúcio <rafaellucio@pm.me>
2021-07-17 22:28:40 -0300

    https variables docker run commands

--- weatherapi/run-docker.sh
+++ weatherapi/run-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker run -p 5000:80 weatherapi:1.0
+docker run -p 5000:80 -p 5001:443 -e ASPNETCORE_URLS="https://+;http://+" -e ASPNETCORE_HTTPS_PORT=5001 -e ASPNETCORE_ENVIRONMENT=Development weatherapi:1.0

8342727d90e1c20e46004f33b03d7e6c586736c0
Rafael Lúcio <rafaellucio@pm.me>
2021-07-25 20:05:45 -0300

    Check if `calendarEL` is `null`

--- AppointmentScheduling/AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -6,21 +6,23 @@ function InitializeCalendar() {
     try {
 
         var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth',
-          headerToolbar: {
-              left: 'prev,next,today',
-              center: 'title', 
-              right: 'dayGridMonth,timeGridWeek,timeGridDay'
-          },
-          selectable: true,
-          editable: false,
-          select: function (event) {
-              onShowModal(event, null);
-          }
-        });
-
-        calendar.render();
+        if (calendar != null) {
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+              initialView: 'dayGridMonth',
+              headerToolbar: {
+                  left: 'prev,next,today',
+                  center: 'title', 
+                  right: 'dayGridMonth,timeGridWeek,timeGridDay'
+              },
+              selectable: true,
+              editable: false,
+              select: function (event) {
+                  onShowModal(event, null);
+              }
+            });
+    
+            calendar.render();
+        }
     }
     catch (e) {
         alert(e);

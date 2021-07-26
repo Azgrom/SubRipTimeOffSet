3029ab824c4b4988a846b1c23b0f46e14d43f849
Rafael Lúcio <rafaellucio@pm.me>
2021-07-25 22:02:38 -0300

    `AppointmentAPIController`

---
+++ AppointmentScheduling/AppointmentScheduling/Controllers/API/AppointmentAPIController.cs
@@ -0,0 +1,30 @@
+﻿using AppointmentScheduling.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AppointmentScheduling.Controllers
+{
+    [Route("API/Appointment")]
+    [ApiController]
+    public class AppointmentAPIController : Controller
+    {
+        private readonly IAppointmentService _appointment_service;
+        private readonly IHttpContextAccessor _http_context_accessor;
+        private readonly string login_user_id;
+        private readonly string role;
+
+        public AppointmentAPIController(IAppointmentService appointment_service, 
+                                        IHttpContextAccessor http_context_accessor)
+        {
+            _appointment_service = appointment_service;
+            _http_context_accessor = http_context_accessor;
+            login_user_id = _http_context_accessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            role = _http_context_accessor.HttpContext.User.FindFirstValue(ClaimTypes.Role);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

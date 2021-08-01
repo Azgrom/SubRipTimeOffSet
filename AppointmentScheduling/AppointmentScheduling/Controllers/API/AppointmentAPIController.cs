c9ee7d8a7e31a1f705c312e7c2f83ecd3fe3bd56
Rafael Lúcio <rafaellucio@pm.me>
2021-07-31 22:34:19 -0300

    Rename to `_<variable_name>`

--- AppointmentScheduling/AppointmentScheduling/Controllers/API/AppointmentAPIController.cs
+++ AppointmentScheduling/AppointmentScheduling/Controllers/API/AppointmentAPIController.cs
@@ -14,16 +14,16 @@ namespace AppointmentScheduling.Controllers
     {
         private readonly IAppointmentService _appointment_service;
         private readonly IHttpContextAccessor _http_context_accessor;
-        private readonly string login_user_id;
-        private readonly string role;
+        private readonly string _loginUserId;
+        private readonly string _role;
 
         public AppointmentAPIController(IAppointmentService appointment_service, 
                                         IHttpContextAccessor http_context_accessor)
         {
             _appointment_service = appointment_service;
             _http_context_accessor = http_context_accessor;
-            login_user_id = _http_context_accessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
-            role = _http_context_accessor.HttpContext.User.FindFirstValue(ClaimTypes.Role);
+            _loginUserId = _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            _role = _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.Role);
         }
 
         [HttpPost]

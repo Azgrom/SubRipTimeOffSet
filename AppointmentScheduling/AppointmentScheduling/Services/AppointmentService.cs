e04307ac1f2135abd11cc6cb5d3663644cfa4ce3
Rafael Lúcio <rafaellucio@pm.me>
2021-07-31 21:12:31 -0300

    Change snake cased variable names to camelCase

--- AppointmentScheduling/AppointmentScheduling/Services/AppointmentService.cs
+++ AppointmentScheduling/AppointmentScheduling/Services/AppointmentService.cs
@@ -19,8 +19,8 @@ namespace AppointmentScheduling.Services
 
         public async Task<int> AddUpdate(AppointmentViewModel model)
         {
-            var start_date = DateTime.Parse(model.StartDate);
-            var end_date = DateTime.Parse(model.StartDate).AddMinutes(Convert.ToDouble(model.Duration));
+            var startDate = DateTime.Parse(model.StartDate);
+            var endDate = DateTime.Parse(model.StartDate).AddMinutes(Convert.ToDouble(model.Duration));
 
             if (model != null && model.Id > 0)
             {
@@ -34,8 +34,8 @@ namespace AppointmentScheduling.Services
                 {
                     Title = model.Title,
                     Description = model.Description,
-                    StartDate = start_date,
-                    EndDate = end_date,
+                    StartDate = startDate,
+                    EndDate = endDate,
                     Duration = model.Duration,
                     DoctorId = model.DoctorId,
                     PatientId = model.PatientId,
@@ -53,8 +53,8 @@ namespace AppointmentScheduling.Services
         public List<DoctorViewModel> GetDoctorList()
         {
             var doctors = (from user in _db.Users
-                           join user_roles in _db.UserRoles on user.Id equals user_roles.UserId
-                           join roles in _db.Roles.Where(x => x.Name == Helper.Doctor) on user_roles.RoleId equals roles.Id
+                           join userRoles in _db.UserRoles on user.Id equals userRoles.UserId
+                           join roles in _db.Roles.Where(x => x.Name == Helper.Doctor) on userRoles.RoleId equals roles.Id
                            select new DoctorViewModel
                            {
                                Id = user.Id,
@@ -68,8 +68,8 @@ namespace AppointmentScheduling.Services
         public List<PatientViewModel> GetPatientList()
         {
             var patients = (from user in _db.Users
-                           join user_roles in _db.UserRoles on user.Id equals user_roles.UserId
-                           join roles in _db.Roles.Where(x => x.Name == Helper.Patient) on user_roles.RoleId equals roles.Id
+                           join userRoles in _db.UserRoles on user.Id equals userRoles.UserId
+                           join roles in _db.Roles.Where(x => x.Name == Helper.Patient) on userRoles.RoleId equals roles.Id
                            select new PatientViewModel
                            {
                                Id = user.Id,

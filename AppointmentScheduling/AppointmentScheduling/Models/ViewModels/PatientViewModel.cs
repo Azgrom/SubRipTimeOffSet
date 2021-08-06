d992e4e568b92505d4f333ec6205e397c8531077
Rafael Lúcio <rafaellucio@pm.me>
2021-08-06 10:07:38 -0300

    `PatientViewModel` class doc comment

--- AppointmentScheduling/AppointmentScheduling/Models/ViewModels/PatientViewModel.cs
+++ AppointmentScheduling/AppointmentScheduling/Models/ViewModels/PatientViewModel.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace AppointmentScheduling.Models.ViewModels
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class PatientViewModel
     {
         public string Id { get; set; }

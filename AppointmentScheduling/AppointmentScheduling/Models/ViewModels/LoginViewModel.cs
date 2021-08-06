df8e20ceeeab3eb639a3e1a414328455882949a9
Rafael Lúcio <rafaellucio@pm.me>
2021-08-06 10:07:22 -0300

    `LoginViewModel` class doc comment

--- AppointmentScheduling/AppointmentScheduling/Models/ViewModels/LoginViewModel.cs
+++ AppointmentScheduling/AppointmentScheduling/Models/ViewModels/LoginViewModel.cs
@@ -2,6 +2,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AppointmentScheduling.Models.ViewModels
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class LoginViewModel
     {
         [Required]

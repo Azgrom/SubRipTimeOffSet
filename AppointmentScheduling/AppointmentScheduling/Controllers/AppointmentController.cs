54dd06359c084504fad4dabdce2916ac6f1e504b
Rafael Lúcio <rafaellucio@pm.me>
2021-08-05 22:25:48 -0300

    `AppointmentController` class doc comment

--- AppointmentScheduling/AppointmentScheduling/Controllers/AppointmentController.cs
+++ AppointmentScheduling/AppointmentScheduling/Controllers/AppointmentController.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 
 namespace AppointmentScheduling.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class AppointmentController : Controller
     {
         /// <summary>
